d2301b8dc7d0e415d9ba2f76ab049eca57c4fa0a
Florian DANIEL ~ Facyla <i.daniel.florian@gmail.com>
2018-10-29 18:17:48 +0100

    Realeasing survey for Elgg 1.8

---
+++ actions/survey/delete.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Elgg Survey plugin
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ */
+
+// Get input data
+$guid = (int) get_input('guid');
+
+// Make sure we actually have permission to edit
+$survey = get_entity($guid);
+
+if (elgg_instanceof($survey, 'object', 'survey') && $survey->canEdit()) {
+	// Get container
+	$container = $survey->getContainerEntity();
+	// Delete the survey!
+	$survey->deleteQuestions();
+	if ($survey->delete()) {
+		// Success message
+		system_message(elgg_echo("survey:deleted"));
+	} else {
+		register_error(elgg_echo("survey:notdeleted"));
+	}
+	// Forward to the main survey page
+	if (elgg_instanceof($container, 'group')) {
+		forward("survey/group/" . $container->guid);
+	} else {
+		forward("survey/owner/" . $container->username);
+	}
+}
+
+forward(REFERER);
+
